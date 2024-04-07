1413f2a91f3836a6e94038ee53ec20a45bb6845c
geraldkisilu <muligerald6@gmail.com>
2024-04-07 19:13:27 +0300

    change 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 // Your code here
+// Your code here
+document.addEventListener('DOMContentLoaded'), () => {
+    const baseURL = 'http://localhost:3000';
+
+    const fetchMovieDetails = async (id) => {
+        const response = await fetch(`${baseURL}/films/${id}`);
+        return await response.json();
+    };
+
+    const fetchAllMovies = async () => {
+        const response = await fetch(`${baseURL}/films`);
+        return await response.json();
+    };
+
+}
